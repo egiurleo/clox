b034c634048e08fb8f646a6449334177ff7f4712
Emily Giurleo <emily.giurleo@shopify.com>
2022-06-03 16:52:40 -0400

    Chapter 15.2

--- vm.c
+++ vm.c
@@ -6,12 +6,26 @@
 
 VM vm;
 
-void initVM() {
+static void resetStack() {
+  vm.stackTop = vm.stack;
+}
 
+void initVM() {
+  resetStack();
 }
 
 void freeVM() {
 
+}
+
+void push(Value value) {
+  *vm.stackTop = value;
+  vm.stackTop++;
+}
+
+Value pop() {
+  vm.stackTop--;
+  return *vm.stackTop;
 }
 
 static InterpretResult run() {
@@ -20,6 +34,14 @@ static InterpretResult run() {
 
   for (;;){
     #ifdef DEBUG_TRACE_EXECUTION
+      printf("       ");
+      for (Value* slot = vm.stack; slot < vm.stackTop; slot++) {
+        printf("[ ");
+        printValue(*slot);
+        printf(" ]");
+      }
+      printf("\n");
+
       disassembleInstruction(vm.chunk, (int)(vm.ip - vm.chunk->code));
     #endif
 
@@ -27,11 +49,14 @@ static InterpretResult run() {
     switch (instruction = READ_BYTE()) {
       case OP_CONSTANT: {
         Value constant = READ_CONSTANT();
+        push(constant);
         printValue(constant);
         printf("\n");
         break;
       }
       case OP_RETURN: {
+        printValue(pop());
+        printf("\n");
         return INTERPRET_OK;
       }
     }
